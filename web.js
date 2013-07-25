7af2f6ca14b3f5b1388bec50d2aab013d67f22a7
Kiran Goud <kirangoud.v@gmail.com>
2013-07-25 16:16:36 +0000

    Updated with File read

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var data = fs.readFileSync('index.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
