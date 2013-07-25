a4153a82140a6dad99b292028870fb20806d0c71
Kiran Goud <kirangoud.v@gmail.com>
2013-07-25 16:25:01 +0000

    Test check

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var data = fs.readFileSync('index.html', 'utf8');
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send('Hello Dude !');
 });
 
 var port = process.env.PORT || 5000;
